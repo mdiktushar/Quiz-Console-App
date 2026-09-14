aaa7eeeae9483ed014d1e70e734c158a07b3f2a3
mdiktushar <md.tushar1804@gmail.com>
2026-09-14 18:46:46 +0600

    score calculate

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         QuestionService questionService = new QuestionService();
-        questionService.displayQuestons();
+        questionService.playQuiz();
+        questionService.printScore();
     }
 }
